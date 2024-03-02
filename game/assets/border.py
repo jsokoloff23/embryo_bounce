4ecf4be35e219aedf837e70e7c643902da532e19
jsokoloff23 <jonahs@uoregon.edu>
2024-03-01 21:15:34 -0800

    added Borders class

--- game/assets/border.py
+++ game/assets/border.py
@@ -1,4 +1,7 @@
+from utils import constants
+
 class Border(object):
+    DEF_SHORT = 20
     """
     border object used in game.
 
@@ -23,3 +26,22 @@ class Border(object):
         self.height: int = height
         self.width: int = width
         self.color: tuple[int, int, int] = color
+
+
+class Borders(object):
+    def __init__(self):
+        self.top = Border(x=0, 
+                          y=0, 
+                          height=Border.DEF_SHORT, 
+                          width=constants.GAME_X_SIZE, 
+                          color=constants.RED)
+        self.back = Border(x=constants.GAME_X_SIZE-Border.DEF_SHORT, 
+                           y=0, 
+                           height=constants.GAME_Y_SIZE, 
+                           width=Border.DEF_SHORT, 
+                           color=constants.BLUE)
+        self.bot = Border(x=0, 
+                          y=constants.GAME_Y_SIZE-Border.DEF_SHORT, 
+                          height=Border.DEF_SHORT, 
+                          width=constants.GAME_X_SIZE, 
+                          color=constants.GREEN)
