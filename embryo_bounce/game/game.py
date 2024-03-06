41a837fb2b3a114c1f5d90eecfa42f1f0e477794
jsokoloff23 <jonahs@uoregon.edu>
2024-03-05 17:35:15 -0800

    separated manager functions to separate classes

--- embryo_bounce/game/game.py
+++ embryo_bounce/game/game.py
@@ -1,21 +1,16 @@
 """
 This module contains the Game class which is the main class created to start
 game
-
-TODO This could be split into multiple classes. Each mode could be made 
-into its own separate class.
 """
-
-import sys
 
 import pygame
 
-from game.mode import Mode
 from game.assets.ball import Ball
 from game.assets.border import Borders
 from game.assets.paddle import Paddle
 from game.managers import DisplayManager, PositionManager, CollisionManager, HighScoreManager, SoundManager
-from utils import constants
+from game.mode import Mode
+from game.mode_classes import Gameplay, HighScores, Exit, MainMenu, HighScoreEntry, PlayAgain
 from utils.camera import HandCam
 from utils.hand_detection import HandDetector
 
@@ -32,16 +27,6 @@ class Game(object):
     main()
         Initializes webcam and hand detection, and then starts game loop
     """
-    FRAMERATE = 60
-    INIT_LIVES = 3
-    #number of frames before ball moves at the beginning
-    INITIAL_WAIT_FRAMES = FRAMERATE*2
-    #number of frames before ball respawns
-    NO_BALL_FRAMES = FRAMERATE*2
-    #frames before ball speed increases
-    FRAMES_NO_SPEED = FRAMERATE*5
-    #number of options on menu
-    NUM_MENU_BUTTONS = 3
     def __init__(self):
         #pygame.init() must be called
         pygame.init()
@@ -64,18 +49,6 @@ class Game(object):
             self.hand_detector, self.paddle, self.ball, self.borders, 
             self.collision_manager)
         self.high_score_manager = HighScoreManager()
-        #initialize game settings and variables
-        self.clock = pygame.time.Clock()
-        self.framerate = Game.FRAMERATE
-        self.lives = Game.INIT_LIVES
-        self.frame_num = 0
-        self.frames_since_speed = 0
-        self.frames_no_ball = 0
-        self.is_ball_gone = False
-        self.score = 0
-        self.name = ""
-        self.try_again = ""
-        self.button_num = 0
 
     def main(self):
         """
@@ -85,24 +58,41 @@ class Game(object):
         self.hand_detector.set_landmarker()
         self.hand_cam.start()
         self.sound_manager.play_menu_song()
-        self._initialize_game_variables()
         while True:
             #Depending on mode of game, selects display and event management.
-            if self.mode == Mode.GAME:
-                self._manage_game_mode()
+            if self.mode == Mode.GAMEPLAY:
+                gameplay = Gameplay(ball=self.ball,
+                                    paddle=self.paddle,
+                                    borders=self.borders,
+                                    sound_manager=self.sound_manager,
+                                    display_manager=self.display_manager,
+                                    collision_manager=self.collision_manager,
+                                    position_manager=self.position_manager)
+                score = gameplay.manage_events()
+                mode = self._get_post_score_mode(score)
             elif self.mode == Mode.HIGH_SCORES:
-                self._manage_hs_mode()
+                high_scores = HighScores(display_manager=self.display_manager,
+                                         high_score_manager=self.high_score_manager)
+                high_scores.manage_events()
+                mode = Mode.MAIN_MENU
             elif self.mode == Mode.EXIT:
-                self._manage_exit_mode()
+                exit = Exit(self.hand_cam)
+                exit.manage_events()
             elif self.mode == Mode.MAIN_MENU:
-                self._manage_menu_mode()
+                main_menu = MainMenu(self.display_manager)
+                mode = main_menu.manage_events()
             elif self.mode == Mode.HIGH_SCORE_ENTRY:
-                self._manage_hs_entry_mode()
+                hs_entry = HighScoreEntry(display_manager=self.display_manager,
+                                          high_score_manager=self.high_score_manager,
+                                          score=score)
+                hs_entry.manage_events()
+                mode = Mode.PLAY_AGAIN
             elif self.mode == Mode.PLAY_AGAIN:
-                self._manage_play_again_mode()
+                play_again = PlayAgain(self.display_manager)
+                mode = play_again.manage_events()
+            self._change_mode(mode)
             #clock tick is to increment variables and run game at consistent
             #frame rate
-            self._tick_clock()
 
     def _change_mode(self, mode):
         """
@@ -112,168 +102,26 @@ class Game(object):
         #high scores and menu
         if self.mode == Mode.HIGH_SCORES:
             pass
-        elif mode == Mode.GAME or mode == Mode.MAIN_MENU:
-            self._initialize_game_variables()
+        elif mode == Mode.GAMEPLAY or mode == Mode.MAIN_MENU:
             self.sound_manager.update_music(mode)
         self.mode = mode
 
-    def _get_key_events(self):
+    def _get_post_score_mode(self, score: int | None):
         """
-        returns pygame events that are key presses
-        """
-        return [event for event in pygame.event.get() if event.type == pygame.KEYDOWN]
-                
-    def _increase_ball_speed(self):
-        """
-        increase ball speed by constants.SPEED_INCREMENT
-        """
-        if self.frames_since_speed > Game.FRAMES_NO_SPEED:
-            self.ball.speed += constants.SPEED_INCREMENT
-            self.frames_since_speed = 0
+        returns mode based on score.
 
-    def _is_ball_gone_update(self):
-        """
-        Updates is_ball_gone flag to position_manager.is_ball_gone().
-        If is_ball_gone is True, plays mistake sound.
-        """
-        self.is_ball_gone = self.position_manager.is_ball_gone()
-        if self.is_ball_gone:
-            self.sound_manager.play_mistake_sound()
+        If score is an int and a high score, returns HIGH_SCORE_ENTRY.
 
-    def _manage_ball_gone(self):
-        """
-        event manager when ball is gone
-        """
-        self.position_manager.update(False)
-        self.display_manager.game_update(self.lives, self.score)
-        if self.frames_no_ball > Game.NO_BALL_FRAMES:
-            self.lives -= 1
-            self._spawn_new_ball()
-            self.is_ball_gone = False
+        If score is an int and not a high score, returns PLAY_AGAIN.
 
-    def _manage_exit_mode(self):
+        If score is None, returns MAIN_MENU.
         """
-        event manager when mode is EXIT
-        """
-        #For some reason, cam feed doesn't die on sys.exit(), so stop manually
-        self.hand_cam.stop_stream()
-        pygame.quit()
-        sys.exit()
-
-    def _manage_game_mode(self):
-        """
-        event manager when mode is GAME
-        """
-        for event in self._get_key_events():
-            #escape returns to main menu
-            if event.key == pygame.K_ESCAPE:
-                self._change_mode(Mode.MAIN_MENU)
-        if self.lives:
-            #waits at the beginning of the game for ball to start moving
-            if self.frame_num < Game.INITIAL_WAIT_FRAMES:
-                self.score = 0
-                self.position_manager.update(False)
-                self.display_manager.game_update(self.lives, self.score)
-            #normal game loop with ball moving
-            else:
-                if not self.is_ball_gone:
-                    self.position_manager.update()
-                    self.display_manager.game_update(self.lives, self.score)
-                    self._increase_ball_speed()
-                    self.frames_no_ball = 0
-                    self._is_ball_gone_update()
-                #when ball disappears, use manage_ball_gone() to manage events
-                else:
-                    self._manage_ball_gone()
-        #when lives are out, either enter high score or go to play again.
+        if score is None:
+            mode = Mode.MAIN_MENU
         else:
-            if self.high_score_manager.is_high_score(self.score):
-                self._change_mode(Mode.HIGH_SCORE_ENTRY)
+            if self.high_score_manager.is_high_score(score):
+                mode = Mode.HIGH_SCORE_ENTRY
             else:
-                self._change_mode(Mode.PLAY_AGAIN)
-
-    def _manage_hs_entry_mode(self):
-        self.display_manager.high_score_entry_update(self.name)
-        for event in self._get_key_events():
-            if event.key == pygame.K_RETURN:
-                #on RETURN press, submits score and changes mode to PLAY_AGAIN.
-                self.high_score_manager.write_new_high_score(
-                    self.name, self.score)
-                self._change_mode(Mode.PLAY_AGAIN)
-            elif event.key == pygame.K_BACKSPACE:
-                #removes character on backspace
-                self.name = self.name[:-1]
-            else:
-                #adds character
-                if len(self.name) <= constants.NAME_CHARACTER_LIMIT:
-                    self.name += event.unicode
-
-    def _manage_hs_mode(self):
-        entries = self.high_score_manager.get_entries()
-        self.display_manager.high_score_update(entries)
-        for event in self._get_key_events():
-            if event.key == pygame.K_ESCAPE:
-                self._change_mode(Mode.MAIN_MENU)
-
-    def _manage_play_again_mode(self):
-        self.display_manager.play_again_update(self.try_again)
-        for event in self._get_key_events():
-            if event.key == pygame.K_RETURN:
-                #on enter press, if y or Y is entered, start new game
-                if self.try_again in ["y", "Y"]:
-                    self._change_mode(Mode.GAME)
-                #on enter press, if y or Y is entered, start new game
-                elif self.try_again in ["n", "N"]:
-                    self._change_mode(Mode.MAIN_MENU)
-                #if neither y nor were entered, reset prompt
-                else:
-                    self.try_again = ""
-            elif event.key == pygame.K_BACKSPACE:
-                #removes character on backspace
-                self.try_again = self.try_again[:-1]
-            else:
-                #addes character
-                self.try_again += event.unicode
-
-    def _manage_menu_mode(self):
-        self.display_manager.menu_update(self.button_num)
-        for event in self._get_key_events():
-            if event.key == pygame.K_DOWN:
-                #make sure user can only select button that exists
-                if self.button_num < Game.NUM_MENU_BUTTONS:
-                    self.button_num += 1
-            elif event.key == pygame.K_UP:
-                #make sure user can only select button that exists
-                if self.button_num != 0:
-                    self.button_num -= 1
-            elif event.key == pygame.K_RETURN:
-                #Menu mode selection lines up with Mode enums, so button_num
-                #selects correct mode
-                self._change_mode(Mode(self.button_num))
-
-    def _initialize_game_variables(self):
-        self.framerate = Game.FRAMERATE
-        self.lives = Game.INIT_LIVES
-        self.frame_num = 0
-        self.frames_since_speed = 0
-        self.frames_no_ball = 0
-        self.is_ball_gone = False
-        self.score = 0
-        self.name = ""
-        self.try_again = ""
-        self.button_num = 0
-
-    def _spawn_new_ball(self):
-        self.ball = Ball()
-        #TODO make an event that auto updates ball in managers.
-        self.display_manager.ball = self.ball
-        self.position_manager.ball = self.ball
-        self.collision_manager.ball = self.ball
-                    
-    def _tick_clock(self):
-        #tick(framerate) keeps game at consistent framerate.
-        self.clock.tick(self.framerate)
-        self.frame_num += 1
-        self.frames_since_speed += 1
-        self.frames_no_ball += 1
-        self.score += 1
+                mode = Mode.PLAY_AGAIN
+        return mode
+    
---
+++ embryo_bounce/game/mode_classes.py
@@ -0,0 +1,243 @@
+"""
+This module contains classes that manage events based on the mode
+set within the main game loop. There's one for each enum in the mode
+Enum class.
+"""
+
+import sys
+
+import pygame
+
+from game.assets.ball import Ball
+from game.assets.border import Borders
+from game.assets.paddle import Paddle
+from game.managers import DisplayManager, PositionManager, CollisionManager, HighScoreManager, SoundManager
+from game.mode import Mode
+from utils import constants
+from utils.camera import HandCam
+
+
+class Gameplay():
+    """
+    manages events while in GAMEPLAY mode
+    """
+    FRAMERATE = 60
+    INIT_LIVES = 3
+    #number of frames before ball moves at the beginning
+    INITIAL_WAIT_FRAMES = FRAMERATE*2
+    #number of frames before ball respawns
+    NO_BALL_FRAMES = FRAMERATE*2
+    #frames before ball speed increases
+    FRAMES_NO_SPEED = FRAMERATE*5
+    def __init__(self, 
+                 ball: Ball, 
+                 paddle: Paddle, 
+                 borders: Borders, 
+                 position_manager: PositionManager, 
+                 display_manager: DisplayManager, 
+                 sound_manager: SoundManager, 
+                 collision_manager: CollisionManager):
+        self.ball = ball
+        self.paddle = paddle
+        self.borders = borders
+        self.position_manager = position_manager
+        self.display_manager = display_manager
+        self.sound_manager = sound_manager
+        self.collision_manager = collision_manager
+        #initialize game settings and variables
+        self.clock = pygame.time.Clock()
+        self.framerate = Gameplay.FRAMERATE
+        self.lives = Gameplay.INIT_LIVES
+        self.frame_num = 0
+        self.frames_since_speed = 0
+        self.frames_no_ball = 0
+        self.is_ball_gone = False
+        self.score = 0
+    
+    def manage_events(self):
+        while self.lives != 0:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    #escape returns to main menu
+                    if event.key == pygame.K_ESCAPE:
+                        return None
+                    #waits at the beginning of the game for ball to start moving
+            if self.frame_num < Gameplay.INITIAL_WAIT_FRAMES:
+                self.score = 0
+                self.position_manager.update(False)
+                self.display_manager.game_update(self.lives, self.score)
+            #normal game loop with ball moving
+            else:
+                if not self.is_ball_gone:
+                    self.position_manager.update()
+                    self.display_manager.game_update(self.lives, self.score)
+                    self._increase_ball_speed()
+                    self.frames_no_ball = 0
+                    self._is_ball_gone_update()
+                #when ball disappears, use manage_ball_gone() to manage events
+                else:
+                    self._manage_ball_gone()
+            self._tick_clock()
+        return self.score
+
+    def _increase_ball_speed(self):
+        """
+        increase ball speed by constants.SPEED_INCREMENT
+        """
+        if self.frames_since_speed > Gameplay.FRAMES_NO_SPEED:
+            self.ball.speed += constants.SPEED_INCREMENT
+            self.frames_since_speed = 0
+
+    def _is_ball_gone_update(self):
+        """
+        Updates is_ball_gone flag to position_manager.is_ball_gone().
+        If is_ball_gone is True, plays mistake sound.
+        """
+        self.is_ball_gone = self.position_manager.is_ball_gone()
+        if self.is_ball_gone:
+            self.sound_manager.play_mistake_sound()
+    
+    def _manage_ball_gone(self):
+        """
+        event manager when ball is gone
+        """
+        self.position_manager.update(False)
+        self.display_manager.game_update(self.lives, self.score)
+        if self.frames_no_ball > Gameplay.NO_BALL_FRAMES:
+            self.lives -= 1
+            self._spawn_new_ball()
+            self.is_ball_gone = False
+
+    def _spawn_new_ball(self):
+        self.ball = Ball()
+        #TODO make an event that auto updates ball in managers.
+        self.display_manager.ball = self.ball
+        self.position_manager.ball = self.ball
+        self.collision_manager.ball = self.ball
+                    
+    def _tick_clock(self):
+        #tick(framerate) keeps game at consistent framerate.
+        self.clock.tick(self.framerate)
+        self.frame_num += 1
+        self.frames_since_speed += 1
+        self.frames_no_ball += 1
+        self.score += 1
+
+
+class HighScores():
+    """
+    manages events while in HIGH_SCORES mode
+    """
+    def __init__(self, 
+                 display_manager: DisplayManager, 
+                 high_score_manager: HighScoreManager):
+        self.display_manager = display_manager
+        self.high_score_manager = high_score_manager
+
+    def manage_events(self):
+        entries = self.high_score_manager.get_entries()
+        #just a still image since it's only updated once!
+        self.display_manager.high_score_update(entries)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return Mode.MAIN_MENU
+
+
+class Exit():
+    def __init__(self, hand_cam: HandCam):
+        self.hand_cam = hand_cam
+
+    def manage_events(self):
+        self.hand_cam.stop_stream()
+        pygame.quit()
+        sys.exit()
+
+
+class MainMenu():
+    """
+    manages events while in MAIN_MENU mode
+    """
+    NUM_MENU_BUTTONS = 3
+    def __init__(self, display_manager: DisplayManager):
+        self.display_manager = display_manager
+        self.button_num = 0
+
+    def manage_events(self):
+        while True:
+            self.display_manager.menu_update(self.button_num)
+            for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_DOWN:
+                            #make sure user can only select button that exists
+                            if self.button_num < MainMenu.NUM_MENU_BUTTONS:
+                                self.button_num += 1
+                        elif event.key == pygame.K_UP:
+                            #make sure user can only select button that exists
+                            if self.button_num != 0:
+                                self.button_num -= 1
+                        elif event.key == pygame.K_RETURN:
+                            #menu mode selection lines up with Mode enums, so 
+                            #button_numselects correct mode
+                            return Mode(self.button_num)
+                        
+class HighScoreEntry():
+    """
+    manages events while in HIGH_SCORE_ENTRY mode
+    """
+    def __init__(self, display_manager:DisplayManager, 
+                 high_score_manager: HighScoreManager,
+                 score: int):
+        self.display_manager = display_manager
+        self.high_score_manager = high_score_manager
+        self.score = score
+        self.name = ""
+
+    def manage_events(self):
+        while True:
+            self.display_manager.high_score_entry_update(self.name)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        #on RETURN press, submits score
+                        self.high_score_manager.write_new_high_score(
+                            self.name, self.score)
+                        return
+                    elif event.key == pygame.K_BACKSPACE:
+                        #removes character on backspace
+                        self.name = self.name[:-1]
+                    else:
+                        #adds character
+                        if len(self.name) <= constants.NAME_CHARACTER_LIMIT:
+                            self.name += event.unicode
+
+class PlayAgain():
+    """
+    manages events while in PLAY_AGAIN mode
+    """
+    def __init__(self, display_manager: DisplayManager):
+        self.display_manager = display_manager
+        self.try_again = ""
+
+    def manage_events(self):
+        while True:
+            self.display_manager.play_again_update(self.try_again)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        #on enter press, if y or Y is entered, start new game
+                        if self.try_again in ["y", "Y"]:
+                            return Mode.GAMEPLAY
+                        #on enter press, if y or Y is entered, start new game
+                        elif self.try_again in ["n", "N"]:
+                            return Mode.MAIN_MENU
+                        #if neither y nor were entered, reset prompt
+                        else:
+                            self.try_again = ""
+                    elif event.key == pygame.K_BACKSPACE:
+                        #removes character on backspace
+                        self.try_again = self.try_again[:-1]
+                    else:
+                        #addes character
+                        self.try_again += event.unicode
